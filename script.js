c151dcf329f47112ddd04e287d81d8ae79483d6d
MDMwakulo <mdmwakulo@gmail.com>
2023-06-13 12:07:45 -0700

    Hoisting and Temporal Dead Zone (TDZ) in Practice

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Scoping in Practice
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
 
@@ -37,3 +38,48 @@ const firstName = 'Durell';
 calcAge(1995);
 // console.log(age);
 // printAge();
+
+// Hoising and Temporal Dead Zone (TDZ) in Practice
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Durell';
+let job = 'student';
+const year = 1995;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3)); // undefined(2,3) --> Uncaught TypeError: addArrow is not a function
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
